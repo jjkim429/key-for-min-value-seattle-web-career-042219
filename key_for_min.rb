1c7d2e15d494b54cb26ef4537919841121944a97
Jennifer Kim <jjkim429@gmail.com>
2019-04-12 01:41:31 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -9,7 +9,7 @@ def key_for_min_value(name_hash)
     smallest_value = nil
     smallest_key = nil
     name_hash.collect do |key, value|
-      if smallest_value == nil
+      if value < smallest value
     end
   end
 end
